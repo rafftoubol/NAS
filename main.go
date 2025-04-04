3ee8da3d56d02ffe91c8d97a0adcba73569a05d4
Raphael Toubol <raphael.toubol@gmail.com>
2025-04-04 03:49:04 +0300

    CVE Parsing, structuring and general data wrangling... see file for detailed desc, and main.go update

--- main.go
+++ main.go
@@ -6,4 +6,12 @@ import (
 
 func main() {
 	cli.Execute()
+	/* EXAMPLE USAGE OF FETCH CVES
+		TO DO: Proper integration into main
+	if CVEs, err := utils.FetchCVE("https://cve.circl.lu/api/search/vercel/next.js"); err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(CVEs)
+	}
+	*/
 }
