efa1da30cb83dd99b05609f21d625b3e4858bea3
Menny <giovanni.menon.dev@gmail.com>
2025-04-25 01:01:27 +0300

    Fix Error Handling, Core refactor and Logrus Debug/Warn code

--- src/dependencyscan[Deprecated]/versionchecker.go
+++ src/dependencyscan[Deprecated]/versionchecker.go
@@ -1,18 +1,12 @@
 package dependencyscan_Deprecated_
 
-import (
-	"attack-surface/src/utils"
-	"fmt"
-)
-
 type VersionVulns struct {
 	NextVersion string
 	CVEVulnIDs  []string
 }
 
 func checkVersion() {
-	var VersionVulns = &VersionVulns{}
-	VersionVulns.NextVersion = utils.GlobalNext.Version
-	fmt.Println("Next Project Version", VersionVulns.NextVersion)
-	
+	//var VersionVulns = &VersionVulns{}
+	//VersionVulns.NextVersion = utils.GlobalNext.Version
+	//fmt.Println("Next Project Version", VersionVulns.NextVersion)
 }
